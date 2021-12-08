d6b4a12d94b5912ccc01998e03449941bff4de4a
Douglas Lins <douglas_lins11@hotmail.com>
2021-12-07 23:39:01 -0300

    valida inicio da rodada

--- src/test/java/jogowar/domain/JogoTest.java
+++ src/test/java/jogowar/domain/JogoTest.java
@@ -1,19 +1,17 @@
 package jogowar.domain;
 
-import jogowar.domain.Cor;
-import jogowar.domain.Dado;
-import jogowar.domain.Jogador;
 import org.junit.jupiter.api.*;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class JogoTest {
 
     private Dado dado;
     private List<Jogador> jogadores;
+    private Jogo jogo;
 
     @BeforeEach
     void setup() {
@@ -22,12 +20,26 @@ class JogoTest {
     }
 
     @Test
-    void Deve_gerar_um_numero_entre_1_e_6() {
+    void deveGerarUmNumeroEntre1e6() {
         final int maior = 6;
         final int menor = 1;
         int random = dado.rodar();
         assertTrue(maior >= random);
         assertTrue(menor <= random);
+    }
+
+    @Test
+    void deveAutorizarInicializacaoRodada(){
+        jogo = new Jogo(this.dado, this.jogadores);
+
+        assertTrue(jogo.inicializaRodada());
+    }
+
+    @Test
+    void deveInvalidarInicializacaoRodada(){
+        jogo = new Jogo(this.dado, List.of(new Jogador("Almir", Cor.sortear())));
+
+        assertThrows(IllegalArgumentException.class, () -> jogo.inicializaRodada());
     }
 
 
